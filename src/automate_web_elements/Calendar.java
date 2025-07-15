0ccf08a919ecb3de3ab47572a053c6b0f0428e10
GouthamAG <gbhatgoutham@gmail.com>
2025-07-15 17:15:52 +0530

    Calendar

---
+++ src/automate_web_elements/Calendar.java
@@ -0,0 +1,16 @@
+package automate_web_elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Calendar {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.cssSelector(".ui-state-default.ui-state-active")).click();
+
+	}
+
+}
