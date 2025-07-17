2f6a8d196a9a57def19a41ce72b3fb239da38857
GouthamAG <gbhatgoutham@gmail.com>
2025-07-17 22:13:54 +0530

    Explicit Wait

--- src/deep_dive/Base.java
+++ src/deep_dive/Base.java
@@ -1,16 +1,21 @@
 package deep_dive;
 
+import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Base {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 		List<WebElement> products =  driver.findElements(By.xpath("//div[@class='product']"));
 		for(WebElement product:products) {
@@ -19,6 +24,18 @@ public class Base {
 				break;
 			}
 		}
+		
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.xpath("//button[contains(text(), 'PROCEED TO CHECKOUT')]")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		
+//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+		
+		driver.findElement(By.cssSelector("span.promoInfo")).getText();
 
 	}
 
